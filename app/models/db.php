7ac359eabb543dc9d5c00718c40a6550d4cb6d5f
Marcos Oliveira <marcos.oliveira098@gmail.com>
2019-06-04 08:02:21 -0400

    db update

--- app/models/db.php
+++ app/models/db.php
@@ -1,108 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marcos
- * Date: 03/06/2019
- * Time: 17:20
+
+/*
+
+ * Versão 1.0.0 &copy, Divisão de Tecnologia da Informação e Eletrônica - Prefeitura Municipal de Figueirão
+ * Todos os direitos reservados
+ * Code By @MarcosOliveira
  */
-class banco{
-    private $host;
-    private $user;
-    private $pass;
-    private $db;
 
-    /**
-     * banco constructor.
-     * @param string $host
-     * @param string $user
-     * @param string $pass
-     * @param string $db
-     */
-    public function __construct($host, $user, $pass, $db)
-    {
-        $this->host = "127.0.0.1";
-        $this->user = "root";
-        $this->pass = "";
-        $this->db = "acricam";
-    }
-
-
-    /**
-     * @return string
-     */
-    public function getHost()
-    {
-        return $this->host;
-    }
-
-    /**
-     * @param string $host
-     */
-    public function setHost($host)
-    {
-        $this->host = $host;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * @param string $user
-     */
-    public function setUser($user)
-    {
-        $this->user = $user;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPass()
-    {
-        return $this->pass;
-    }
-
-    /**
-     * @param string $pass
-     */
-    public function setPass($pass)
-    {
-        $this->pass = $pass;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDb()
-    {
-        return $this->db;
-    }
-
-    /**
-     * @param string $db
-     */
-    public function setDb($db)
-    {
-        $this->db = $db;
-    }
-
-
-
-    public static function conexao()
-    {
-
-        $link = mysqli_connect($this->getHost(), $this->getUser(), $this->getPass(), $this->getDb());
-        if (!$link) {
-            die("Falha na conexao: " . mysqli_connect_error());
+class conecta{
+    public static function local(){
+        $host   = "127.0.0.1";
+        $user   = "root";
+        $pass   = "";
+        $db     = "law-pro";
+        $link = mysqli_connect($host,$user,$pass,$db);
+        if(!$link){
+            die("Falha na conexao: " . mysqli_connect_error($link));
         }
-
-        mysqli_set_charset($conn, "utf8");
-        return $link;
+        $conn=mysqli_connect($host,$user,$pass,$db);
+        mysqli_set_charset($conn,"utf8");
+        return $conn;
 
     }
 }
